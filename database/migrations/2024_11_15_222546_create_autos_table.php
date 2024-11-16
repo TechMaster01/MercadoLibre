b22e9d14609ff3ccee66524ed9bf222e6b7360a7
JuanAlejandroRO <jar.rodriguez20@info.uas.edu.mx>
2024-11-16 14:58:09 -0700

    Migraciones para Autos y Inmuebles

---
+++ database/migrations/2024_11_15_222546_create_autos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAutosTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('AUTOS', function (Blueprint $table) {
+            $table->id(); 
+            $table->enum('TIPO_VEHICULO', ['Sedán', 'SUV', 'Camioneta', 'Deportivo', 'Hatchback', 'Convertible', 'Otro'])->default('Otro'); 
+            $table->string('MARCA', 100); 
+            $table->string('MODELO', 100); 
+            $table->date('FECHA_PUBLICACION'); 
+            $table->decimal('PRECIO', 10, 2);
+            $table->boolean('FINANCIADO')->default(false); 
+            $table->year('AÑO_VEHICULO'); 
+            $table->integer('KILOMETRAJE')->unsigned(); 
+            $table->text('PREGUNTAS_CLIENTE')->nullable(); 
+            $table->text('CARACTERISTICAS')->nullable(); 
+            $table->text('DESCRIPCION')->nullable(); 
+            $table->text('INFORMACION_VENDEDOR')->nullable(); 
+            $table->timestamps(); 
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('AUTOS');
+    }
+}
---
+++ database/migrations/2024_11_16_215221_create_inmuebles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInmueblesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('INMUEBLES', function (Blueprint $table) {
+            $table->id();
+            $table->string('TIPO_INMUEBLE', 100); 
+            $table->string('TITULO', 255);
+            $table->date('FECHA_PUBLICACION');
+            $table->string('AUTOR_PUBLICACION', 255); 
+            $table->decimal('PRECIO', 15, 2);
+            $table->integer('METROS_INMUEBLE'); 
+            $table->integer('NUMERO_RECAMARAS')->nullable();
+            $table->integer('NUMERO_BAÑOS')->nullable();
+            $table->string('UBICACION', 255); 
+            $table->text('CARACTERISTICAS_INMUEBLE')->nullable(); 
+            $table->text('INFORMACION_ZONA')->nullable(); 
+            $table->text('DESCRIPCION')->nullable();
+            $table->text('INFORMACION_TIENDA')->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('INMUEBLES');
+    }
+}
